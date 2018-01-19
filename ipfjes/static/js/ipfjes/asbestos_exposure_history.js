16d6f7c86ed03a12ef295499704d89cabc92795c
fredkingham <fredkingham@gmail.com>
2018-01-19 15:13:53 +0000

    change to check that if asbestostos exposure is not an array, not with length

--- ipfjes/static/js/ipfjes/asbestos_exposure_history.js
+++ ipfjes/static/js/ipfjes/asbestos_exposure_history.js
@@ -1,6 +1,6 @@
 angular.module('opal.controllers').controller('AsbestosExposureHistoryCtrl', function(scope, step, episode){
     "use strict";
-    if(scope.editing.asbestos_exposure_screening.length){
+    if(_.isArray(scope.editing.asbestos_exposure_screening)){
       scope.editing.asbestos_exposure_screening = _.first(scope.editing.asbestos_exposure_screening);
     }
 });
